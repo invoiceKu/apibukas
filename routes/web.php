1e5fd894329961699b5e3c2153059c564dc67ec2
Andrian <1718054@scholar.itn.ac.id>
2026-02-01 23:22:41 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,24 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 Route::get('/migrate-db', function () {
-    \Artisan::call('migrate', ["--force" => true]);
-    return 'Migrasi Berhasil! Cek database Anda.';
+    try {
+
+        \Artisan::call('optimize:clear');
+        \Artisan::call('config:clear');
+        
+        echo "Cache berhasil dibersihkan.<br>";
+
+        $socket = config('database.connections.mysql.unix_socket');
+        echo "Target Socket: " . ($socket ? $socket : "KOSONG (Masalah disini!)") . "<br>";
+
+        \DB::connection()->getPdo();
+        echo "Tes Koneksi: BERHASIL! <br>";
+
+        \Artisan::call('migrate', ["--force" => true]);
+        return '<b>SUKSES! Tabel berhasil dibuat.</b>';
+        
+    } catch (\Exception $e) {
+        return "ERROR: " . $e->getMessage();
+    }
 });
 Route::get('/pricing', [PricingController::class, 'index']);
